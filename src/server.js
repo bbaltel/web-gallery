fcbb81717e8245dfe713f0befe26ddefb809372b
Maxi Bal <greatnesshere5@gmail.com>
2018-05-12 22:33:16 -0400

    Backend Fixes

--- src/server.js
+++ src/server.js
@@ -1,51 +1,98 @@
 const port = 8080;
-
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
-
-http.createServer(function (req, res) {
+var path = require('path');
+http.createServer(function (req, res) 
+{
     console.log("Requested " + req.url);
-    if (req.url == '/fileupload') {
+    if (req.url == '/fileupload') 
+    {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
+        form.parse(req, function (err, fields, files) 
+        {
             var oldpath = files.filetoupload.path;
             var newpath = `${__dirname}\\web\\assets\\${files.filetoupload.name}`;
-            fs.rename(oldpath, newpath, function (err) {
+            fs.rename(oldpath, newpath, function (err) 
+            {
                 if (err) throw err;
             });
-            fs.readFile('./web/data.json', (err,data) => {
+            fs.readFile('./web/data.json', (err,data) => 
+            {
                 var obj = JSON.parse(data);
                 console.log("Adding " + files.filetoupload.name + " to JSON File");
-                var addition = { "caption": fields.caption, "date": fields.date, "path": newpath };
+                var addition = { "caption": fields.caption, "date": fields.date, "path": files.filetoupload.name };
                 obj.images[obj.images.length + 1] = addition;
                 fs.writeFile(`${__dirname}\\web\\data.json`, JSON.stringify(obj), () => console.log("FINISHED ADDING"));});
             });
             fs.readFile('./web/index.html', (err, data) => {res.write(data, () => res.end())})
-    } else if (req.url == "/add") {
+    } 
+    else if (req.url == "/add") 
+    {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile('./web/add.html', (err,data) => res.write(data, function() {res.end()}));
-    } else if (req.url == "/home" || req.url == "/") {
+    } 
+    else if (req.url == "/home" || req.url == "/") 
+    {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile('./web/index.html', (err,data) => {
             res.write(data, function() {res.end()});
         });   
-    } else if (req.url == "/gallery") {
+    } 
+    else if (req.url == "/gallery") 
+    {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         fs.readFile('./web/gallery.html', (err, data) => {
             res.write(data, function() {res.end()})
         });
-    } else if (req.url == 'gitimg') {
+    } 
+    else if (req.url == '/gitimg') 
+    {
         res.writeHead(200, { 'Content-Type': 'image/png'});
         fs.readFile('./web/img/github.png', (err, data) => res.write(data, () => {res.end()}));
-    } else if (req.url == "favicon") {
-        res.writeHead(200, { 'Content-Type': 'image/icon'});
+    } 
+    else if (req.url == "/favicon") 
+    {
+        res.writeHead(200, { 'Content-Type': 'image/x-icon'});
         fs.readFile('./web/img/favicon.ico', (err,data) => res.write(data, () => res.end()));
-    } else if (req.url == "json") {
+    } 
+    else if (req.url == "/data.json") 
+    {
         res.writeHead(200, {"Content-Type": "text/json"});
         fs.readFile('./web/data.json', (err,data) => res.write(data, () => res.end()));
+    } 
+    else if (req.url == "/gallery") {
+        res.writeHead(200, {"Content-Type": "image/jpg"});
+        fs.readFile("./web/img/gallery.jpg", (err, data) => res.write(data, () => res.end()));
+    } 
+    else if (true)
+    {
+        fs.readFile('./web/data.json', (err, data) => 
+        {
+            var object = JSON.parse(data);
+            object = JSON.parse(JSON.stringify(object).replace("null,", ""));
+            console.log(object);
+            console.log("\n\n\n\n\n\n\n");
+            for (i = 0; i < object.images.length; i++) 
+            {
+                console.log(object.images[i]);
+                console.log(object.images[i].path);
+                if (JSON.stringify(object.images[i].path).endsWith('.png')) 
+                {
+                    res.writeHead(200, {"Content-Type": "image/png"});
+                }
+                else if (JSON.stringify(object.images[i].path).endsWith(".jpg"))
+                {
+                    res.writeHead(200, {'Content-Type': 'image/jpg'});
+                }
+                else if (JSON.stringify(object.images[i].path).endsWith('.heif')) 
+                {
+                    res.writeHead(200, {"Content-Type": "image/heic"});
+                }
+                fs.readFile(`./web/assets/${object.images[i].path}`, (err, data) => res.write(data, () => res.end()));
+            }
+        });
     }
     console.log("Served " + req.url);
 }).listen(port, () => console.log(`Server running on port ${port}, visit localhost:${port}`));
-
